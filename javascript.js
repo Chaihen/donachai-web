c1c727f73e9732d4c5995f9f9947491a67a609ab
Chaihen <gchaihen@gmail.com>
2023-12-16 10:51:05 -0300

    Modificando detalles finales

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1 }    
   slides[slideIndex - 1].style.display = "block";  
-  setTimeout(showSlides, 5000); 
+  setTimeout(showSlides, 4000); 
 }
 
 function comprobarDisponibilidad() {
@@ -25,39 +25,3 @@ function comprobarDisponibilidad() {
   document.getElementById('muestraDisponibilidad').innerHTML = stockComparacion;
 }
 
- const donutsSelect = document.getElementById('donuts');
-      const coberturasSelect = document.getElementById('coberturas');
-      const lluviasSelect = document.getElementById('lluvias');
-      const seleccionDiv = document.getElementById('seleccion');
-    
-      function mostrarSeleccion() {
-        const donutsSeleccionados = [...donutsSelect.selectedOptions].map(option => option.text);
-        const coberturasSeleccionadas = [...coberturasSelect.selectedOptions].map(option => option.text);
-        const lluviasSeleccionadas = [...lluviasSelect.selectedOptions].map(option => option.text);
-    
-        const resultadoHTML = `
-          <h3>Selecciones:</h3>
-          <p><strong>Donuts seleccionados:</strong> ${donutsSeleccionados.length > 0 ? donutsSeleccionados.join(', ') : 'Ninguno'}</p>
-          <p><strong>Coberturas seleccionadas:</strong> ${coberturasSeleccionadas.length > 0 ? coberturasSeleccionadas.join(', ') : 'Ninguna'}</p>
-          <p><strong>Lluvias seleccionadas:</strong> ${lluviasSeleccionadas.length > 0 ? lluviasSeleccionadas.join(', ') : 'Ninguna'}</p>
-        `;
-        seleccionDiv.innerHTML = resultadoHTML;
-      }
-
-      function limpiarSeleccion() {
-    donutsSelect.selectedIndex = -1;
-    coberturasSelect.selectedIndex = -1;
-    lluviasSelect.selectedIndex = -1;
-    seleccionDiv.innerHTML = '';
-  }
-
-  function enviarPedido() {
-    mostrarSeleccion();
-    limpiarSeleccion();
-
-    document.getElementById('seleccionDiv').innerHTML =resultadoHTML
-  }
-       
-      donutsSelect.addEventListener('change', mostrarSeleccion);
-      coberturasSelect.addEventListener('change', mostrarSeleccion);
-      lluviasSelect.addEventListener('change', mostrarSeleccion);
